d2a23a59c029367e1a1aa29a4daba5612ce837d3
stick <stick@localhost.localdomain>
2024-11-25 16:20:08 +0100

    small fixes tried to add molten

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -31,6 +31,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- exit insert in better way
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Move focus to the upper window' })
+
+-- make W and Q work like w and q for my fat fingers
+
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
